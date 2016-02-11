f72867a447f046b3e3901636304c6bb0d6bc57d2
The_Grits <gritsprogramming@gmail.com>
2016-02-11 08:43:09 -0500

    dropdown triangle

--- main.js
+++ main.js
@@ -38,5 +38,5 @@ var sdsDescriptionGerman2;
 
 // when the user selects an option from the dropdown menu
 function languageSelect(l){
-  document.getElementById('dropButton').innerHTML = l; // set the button text to the selected option
+  document.getElementById('dropButton').innerHTML = l + " ▼"; // set the button text to the selected option
 }
