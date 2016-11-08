60b0c55c5a075c08e9b6165a797c4fdf0b671886
Scimonster <tehalmightyscimonster@gmail.com>
2016-11-08 18:13:33 +0200

    Condense language-switching functions into one.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function dropit(){
 // read hash and adjust accordingly on page load
 window.onload = function(){
  if(location.hash){
-   hashSelect(location.hash.substring(1)); 
+   langSelect(location.hash.substring(1));
  }
 };
 
@@ -28,19 +28,19 @@ window.onclick = function(e){
 // creates link objects for the dropdown
 function prep(){
   var dropdown = document.getElementById('dropOptions');
-  
+
   var option;
   for (var i in l10n){
     option = document.createElement('a');
     option.dataset.locale = i;
-    option.onclick = function(){languageSelect(this)};
+    option.onclick = function(){hashSelect(this.getAttribute('data-locale'));};
     option.innerHTML = l10n[i].language;
     dropdown.appendChild(option);
   }
 }
 
-// so that we can select a language according to a URL hash
-function hashSelect(hash){
+// switch language according to its two-letter code
+function langSelect(hash){
   if(l10n[hash]){
     var locale = l10n[hash];
     document.getElementById('dropButton').innerHTML = l10n[hash].language + " ▾"; // set the button text to the selected option
@@ -49,14 +49,4 @@ function hashSelect(hash){
     document.getElementById('uniqueDescription').innerHTML = locale.unique ? locale.unique : l10n['en'].unique;
     document.getElementById('curatorString').innerHTML = locale.curators ? locale.curators : l10n['en'].curators;
   }
-}
-
-// when the user selects an option from the dropdown menu
-function languageSelect(l){
-  var locale = l10n[l.getAttribute('data-locale')];
-  document.getElementById('dropButton').innerHTML = l.innerHTML + " ▾"; // set the button text to the selected option
-  document.getElementById('translatorCredit').innerHTML = locale.translator ? locale.translator : l10n['en'].translator;
-  document.getElementById('standardDescription').innerHTML = locale.standard ? locale.standard : l10n['en'].standard;
-  document.getElementById('uniqueDescription').innerHTML = locale.unique ? locale.unique : l10n['en'].unique;
-  document.getElementById('curatorString').innerHTML = locale.curators ? locale.curators : l10n['en'].curators;
 }
